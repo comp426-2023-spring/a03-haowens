b4ebdb55155b19ee98550b98188127450344febd
Hayley <haowens@email.unc.edu>
2023-04-07 20:44:12 -0400

    debugged rps

--- bin/rps-cli.js
+++ bin/rps-cli.js
@@ -33,56 +33,85 @@ function randomNumber(min, max) {
     return Math.random() * (max - min) + min;
 }
 
-const play = args._[0].toLowerCase();
+// if (args._.length === 0) {
+//     console.log("test 1");
+//     console.log(args[0]);
+// } else if (args._.length === 1) {
+//     console.log("test 2");
+//     console.log(args._[0]);
+// } else if (args._.length === 2) {
+//     console.log("test 3");
+// } else {
+//     console.log("test 4");
+// }
 
-if (args._.length === 1) {
-    let x = randomNumber(1,3);
-    if (x == 1) {
+
+if (args._.length === 0) {
+    let x = Math.floor(Math.random() * 2);
+    if (x === 0) {
         console.log({"player":"rock"});
     } 
-    if (x == 2) {
+    if (x === 1) {
         console.log({"player":"paper"});
     }
-    if (x == 3) {
+    if (x === 2) {
         console.log({"player":"scissors"});
     }
     process.exit(0);
-} else if ((play === "rock")  && args.length() === 2) {
-    let x = randomNumber(1,3);
-    if (x == 1) {
-        console.log({"player":"rock", "opponent":"rock", "result":"tie"});
-    } 
-    if (x == 2) {
-        console.log({"player":"rock", "opponent":"paper", "result":"loss"});
+} else if (args._.length === 1) {
+    let x = Math.floor(Math.random() * 2);
+    const play = args._[0].toLowerCase();
+    if (play === "rock") {
+        if (x === 0) {
+            console.log({"player":"rock", "opponent":"rock", "result":"tie"});
+        } 
+        if (x === 1) {
+            console.log({"player":"rock", "opponent":"paper", "result":"loss"});
+        }
+        if (x === 2) {
+            console.log({"player":"rock", "opponent":"scissors", "result":"win"});
+        }
+        process.exit(0);
+    } else if (play === "paper") {
+        if (x === 0) {
+            console.log({"player":"paper", "opponent":"rock", "result":"win"});
+        } 
+        if (x === 1) {
+            console.log({"player":"paper", "opponent":"paper", "result":"tie"});
+        }
+        if (x === 2) {
+            console.log({"player":"paper", "opponent":"scissors", "result":"loss"});
+        }
+        process.exit(0);
+    } else if (play === "scissors") {
+        if (x === 0) {
+            console.log({"player":"scissors", "opponent":"rock", "result":"loss"});
+        } 
+        if (x === 1) {
+            console.log({"player":"scissors", "opponent":"paper", "result":"win"});
+        }
+        if (x === 2) {
+            console.log({"player":"scissors", "opponent":"scissors", "result":"tie"});
+        }
+        process.exit(0);
+    } else {
+        console.log("Usage: node-rps [SHOT]");
+        console.log("Play Rock Paper Scissors (RPS)");
+        console.log("\n");
+        console.log("   -h, --help      display this help message and exit");
+        console.log("   -r, --rules     display the rules and exit");
+        console.log("\n");
+        console.log("Examples:");
+        console.log('   node-rps        Return JSON with single player RPS result. e.g. {"player":"rock"}');
+        console.log('   node-rps rock   Return JSON with results for RPS played against a simulated opponent. e.g {"player":"rock","opponent":"scissors","result":"win"}');
+        console.log('\n');
+        console.log("Rules for Rock Paper Scissors:");
+        console.log("\n");
+        console.log("   - Scissors CUTS Paper");
+        console.log("   - Paper COVERS Rock");
+        console.log("   - Rock CRUSHES Scissors");
+        process.exit(0);
     }
-    if (x == 3) {
-        console.log({"player":"rock", "opponent":"scissors", "result":"win"});
-    }
-    process.exit(0);
-} else if ((play === "paper")  && args.length() == 2) {
-    let x = randomNumber(1,3);
-    if (x == 1) {
-        console.log({"player":"paper", "opponent":"rock", "result":"win"});
-    } 
-    if (x == 2) {
-        console.log({"player":"paper", "opponent":"paper", "result":"tie"});
-    }
-    if (x == 3) {
-        console.log({"player":"paper", "opponent":"scissors", "result":"loss"});
-    }
-    process.exit(0);
-} else if ((play === "scissors")  && args.length() === 2) {
-    let x = randomNumber(1,3);
-    if (x == 1) {
-        console.log({"player":"scissors", "opponent":"rock", "result":"loss"});
-    } 
-    if (x == 2) {
-        console.log({"player":"scissors", "opponent":"paper", "result":"win"});
-    }
-    if (x == 3) {
-        console.log({"player":"scissors", "opponent":"scissors", "result":"tie"});
-    }
-    process.exit(0);
 } else {
     console.log("Usage: node-rps [SHOT]");
     console.log("Play Rock Paper Scissors (RPS)");
